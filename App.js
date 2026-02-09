26994837b81ddf4bb920793d914f92521a613005
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-02-09 17:28:42 -0500

    Update App.js

--- App.js
+++ App.js
@@ -18,30 +18,12 @@ function App() {
   );
 }
 
-
-*/
-Task 3: Dynamic Content Using JSX and React Components (5 Points)
--Create an array of tasks inside the App component.
--Write a function getRandomTask() that returns a random task from the list.
--Display a random task inside a <h3> using JSX.
 /*
-
-function getRandomTask() {
-  return (
-    <div>
-      <h2>Task:</h2>
-      <h3>{randomtask}</h3>
-    </div>
-  );
-}
-
-
-*/
 Task 4: Parent and Child Components Using JSX (5 Points)
 -Modify App.js to:
 -Import the Greeting, UserInfo, and TaskComponent components.
 -Render them inside a single <div> (following JSX rules).
-/*
+*/
 
 export default App;
 
