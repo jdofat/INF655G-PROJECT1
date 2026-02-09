cb19a0051cae9ca1dbef21cb60ade1a70204e248
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-02-09 17:34:43 -0500

    Update TaskComponent.js

--- components/TaskComponent.js
+++ components/TaskComponent.js
@@ -1,1 +1,18 @@
+/*
+Task 3: Dynamic Content Using JSX and React Components (5 Points)
+-Create an array of tasks inside the App component.
+-Write a function getRandomTask() that returns a random task from the list.
+-Display a random task inside a <h3> using JSX.
+*/
+
+function getRandomTask() {
+  return (
+    <div>
+      <h2>Task:</h2>
+      <h3>{randomtask}</h3>
+    </div>
+  );
+}
+
+export getRandomTask;
 
