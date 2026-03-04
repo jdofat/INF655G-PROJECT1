69783dfc2e5ef40304e6556bce49f723edfb06da
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-03-03 22:01:29 -0500

    Update UserInfo.js

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,10 +1,4 @@
-/* Task 2: Create a Class Component and Use JSX (5 Points)
 
-Create a class-based component named UserInfo that:
--Uses JSX to return a <div> containing a user's name and profession.
--Defines state inside the component to hold user details.
--Displays a random number inside JSX (e.g., "Your lucky number is 7").
-*/
 
 import React from "react";
 
@@ -23,6 +17,9 @@ class UserInfo extends React.Component {
         <h2>User: {this.state.username}</h2>
         <h3>Profession: {this.state.profession}</h3>
         <p>Employee Number: {this.state.employeeNumber}</p>
+        <button onClick={this.props.handleClick}>
+          CLICK ME!
+        </button>
       </div>
     );
   }
