8f1efaeb90630ba5e9151a7461bc21de9dcd9273
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-03-03 21:42:33 -0500

    Update Greeting.js

--- components/Greeting.js
+++ components/Greeting.js
@@ -7,6 +7,17 @@ Task 1: Create a React Component Using JSX (5 Points)
 -Uses JSX to return an <h1> element displaying a greeting message (e.g., "Hello, Welcome to React!").
 -Includes a paragraph <p> with an inline style applied using JSX.
 -Uses JavaScript expressions inside JSX (e.g., displaying the current date dynamically).
+---------------------
+2. Task 1: Use Props to Pass Data (5 Points)
+
+Modify your Greeting component to accept a prop called username.
+Display the username inside the greeting message.
+In App.js, render Greeting twice with different usernames.
+
+Example Output:
+
+Hello, Alice!  
+Hello, Bob!  
   
 */
 
