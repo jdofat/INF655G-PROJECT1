8c688ba248d78ab0c3381251f7071689734ca15d
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-03-03 22:04:24 -0500

    Update TaskForm.js

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -1,1 +1,29 @@
+import { useState } from "react";
+
+function TaskForm() {
+  const [taskName, setTaskName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(taskName);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter Task Name"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+}
+
+
+
+
+
+export default TaskForm;
 
