eed0f953b46ac0d611395d1b83b16c3ab41a1fe7
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-03-27 20:20:13 -0400

    Update TaskForm.js

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -1,29 +1,72 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
-function TaskForm() {
+function TaskForm({tasks, setTasks, deleteTask}) {
   const [taskName, setTaskName] = useState("");
-
+  const [description, setDescription] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(taskName);
+    
+      if (taskName.trim() === "" || description.trim() === "") {
+        alert("enter task and description");
+          return;
+    }
+  
+    setTasks([...tasks, { taskName, description }]);
+    setTaskName("");
+    setDescription("");
+  };
+  
+  const handleSort = () => {
+    setTasks([...tasks].sort((a, b) => a.taskName.localeCompare(b.taskName)));
   };
 
+  const filteredTasks = tasks.filter(task =>
+    task.taskName.toLowerCase().includes(searchTerm.toLowerCase()
+  };
+
+
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Enter Task Name"
-        value={taskName}
-        onChange={(e) => setTaskName(e.target.value)}
-      />
-      <button type="submit">Add Task</button>
-    </form>
-  );
+    <div>
+     <form onSubmit={handleSubmit}>
+        <label>
+         Task:
+           <input
+             type="text"
+             value={taskName}
+             onChange={(e) => setTaskName(e.target.value)
+           }/>
+          </label>
+          
+  <label> Description:
+      <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+       />
+  </label>
+ <button type="submit">submit</button>
+</form>
+
+    <input
+      type="text"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)
+    }/>
+
+    <button onClick={handleSort}> Sort by: Name </button>
+
+    <ul>
+      {filteredTasks.map((task, index) => (
+        <li key={index}>
+          {task.taskName}: 
+          {task.description}
+          (button onClick={( => deleteTask(index)}>delete</button>
+        </li>
+      ))}
+    </ul>
+  </div>
+ );
 }
 
-
-
-
-
-export default TaskForm;
-
+export default TaskForm; 
