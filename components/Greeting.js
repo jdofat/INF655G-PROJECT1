298c7f88207b329f6af18637615bf8bb81d3e2e3
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-03-27 20:26:47 -0400

    Update Greeting.js

--- components/Greeting.js
+++ components/Greeting.js
@@ -1,24 +1,18 @@
 import React, { useState } from 'react';
 
-function isEven(number) {
-  return number % 2 === 0;
-};
-
 function Greeting(props) {
-  const [myGreeting, setGreeting] = useState('Hello');
-  const number = 100;
-  const myDate = new Date().toLocaleDateString();
-  const changeGreeting = () => {
-    setMyGreeting("Hi!");
-  };
-  
+  const [greeting, setGreeting] = useState("Hello");
+  const today = new Date().toLocaleDateString();
+
   return (
     <div>
-      <h1>{myGreeting} {props.username}</h1>
-      <p style={{color: "red"}}>Is 100 an even number?: {isEven(number) ? "Yes" : "No"}</p>
-      <button onClick={changeGreeting}>Change My Greeting</button>
+      <h1>{greeting}, {props.username}!</h1>
+      <p>Today's date: {today}</p>
+      <button onClick={() => setGreeting("Hi")}>
+        Change Greeting
+      </button>
     </div>
-   );
+  );
 }
 
 export default Greeting;
