2103c8848844ef674f40999da9e945c94e8ad456
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-03-27 20:21:05 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,58 +1,72 @@
-import Greeting from "./components/Greeting";
-import UserInfo from "./components/UserInfo";
-import TaskComponent from "./components/TaskComponent";
-import Counter from "./components/Counter";
-import TaskForm from "./components/TaskForm";
+import React, {useState} from 'react';
 
+import Greeting from './INF655-Assignment1/Greeting';   
+import UserInfo from './INF655-Assignment1/UserInfo';   
+import GetRandomTask from './INF655-Assignment1/GetRandomTask';
+import TaskForm from './INF655-Assignment1/TaskForm';
+import Counter from './INF655-Assignment1/Counter';
 
 function App() {
-  const tasknumber = Math.floor(Math.random() * 100);
-  const usernames = ["dwallace", "mscott", "dschrute", "jhalpert"];
-  const professions = ["Employee", "Manager", "Sr. Manager", "CEO"];
+  const [tasks, setTasks] = useState([
+    {taskName: "Read", description: "read book"},
+    {taskName: "Shop", description: "go shopping"},
+    {taskName: "Laundry", description: "do laundry"},
+  ]);
 
-  const randomProfession =
-    professions[Math.floor(Math.random() * professions.length)];
-  
-  const tasks = ["Document", "Collaborate", "Programming", "Coding", "Helping"];
 
-  const handleAlert = () => {
-    alert("Button was clicked!");
+const deleteTask = (taskIndex) => {
+    const areYouSure = window.confirm("are you sure?");
+    if (areYouSure) {
+      setTasks(tasks.filter((task, index) => index !== taskIndex));
+    }
   };
 
-  
   return (
     <div>
-      <h1>{usernames[0]} has:</h1>
-      <p style={{ fontStyle: "italic" }}>
-        {tasknumber} employee tasks.
-      </p>
-
-      <h1>{usernames[1]} has:</h1>
-      <p style={{ fontStyle: "italic" }}>
-        {tasknumber} employee tasks.
-      </p>
-          
-    <div>
-      <Greeting username={usernames[0]} tasknumber={tasknumber} />
-        <Greeting username={usernames[1]} tasknumber={tasknumber} />
-        <UserInfo 
-          profession={randomProfession} 
-          handleClick={handleAlert} 
-        />
-        <TaskComponent tasks={tasks} />
-        <Counter />
-        <TaskForm />
-
-      <h2>Task List:</h2>
-        <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
+      
+      <h1>Task Manager</h1>
+        <TaskForm tasks={tasks} setTasks={setTasks} deleteTask={deleteTask} />
+      
+      <h2>More Tasks</h2>
+        <moreTaskList/>
     </div>
-  </div>
   );
 }
 
-export default App;
+  function handleAlert() {
+    alert ("Alert!");
+  }
 
+  return (
+    <div>
+      <Greeting username="John"/>
+      <Greeting username="Janet"/>
+      <UserInfo name="Jade", job="Student" />
+      {GetRandomTask(tasks)}
+    </div>
+  );
+};
+
+ function moreTaskList() {
+
+   const moreTasks = [
+      "Mow the lawn",
+      "Clean the bathroom",
+      "Polish the floors",
+      "Dust the windows",
+      "Wipe the mirrors"
+    ];
+
+return (
+    <div>
+      <ul>
+        {moreTasks.map((task, index) => (
+          <li key={index}> {task} </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
+export default App;
