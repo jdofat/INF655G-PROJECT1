8ca699ca1d859319279887176747061e962d18ad
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2026-03-27 20:26:57 -0400

    Update UserInfo.js

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,40 +1,31 @@
 import React from 'react';
 
-function UserName({name}) {
-  return <h2>Name: {name}</h2>;
-}
-
-function UserJob({job}) {
-  return <h3>Job: {job}</h3>;
-};
-
 class UserInfo extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        yournumber: Math.floor(Math.random() * 1000),
-        luckynumber: Math.floor(Match.random() * 100) + 1,                  
+    this.state = {
+      luckyNumber: Math.floor(Math.random() * 100) + 1
     };
   }
 
   newLuckyNumber = () => {
     this.setState({
-      luckyNumber: Math.floor(Math.random() * 100) + 1,
-  });
-};
+      luckyNumber: Math.floor(Math.random() * 100) + 1
+    });
+  };
 
   render() {
-    const {name, job, handleAlert} = this.props;
-    const {yournumber, luckyNumber} = this.state;
-
+    const { name, job } = this.props;
+    const { luckyNumber } = this.state;
 
     return (
       <div>
-        <UserName name={name} />
-        <UserJob job={job} />
-        <p>Employee Number: {yournumber}</p>
-        <button onClick={handleAlert}>Show Alert</button>
-        <button onClick={this.newLuckyNumber}>New Number</button>
+        <h2>Name: {name}</h2>
+        <h3>Profession: {job}</h3>
+        <p>Your lucky number is: {luckyNumber}</p>
+        <button onClick={this.newLuckyNumber}>
+          Generate New Lucky Number
+        </button>
       </div>
     );
   }
